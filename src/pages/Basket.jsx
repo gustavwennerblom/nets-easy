4137e7ad75ec4db77fe57187a0f81ef35db3efa1
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2020-02-19 10:47:40 +0100

    With functional redirect on successful payment

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 
 const Basket = () => {
     const history = useHistory();
-    // const [checkout, setCheckout] = useState();
-    let checkout;
+    const searchParams = new URLSearchParams(history.location.search)
+    const paymentIdFromURL = searchParams.get('paymentId')
+
+    const defaultCheckoutOptions = {
+      checkoutKey: process.env.REACT_APP_CHECKOUT_KEY,
+      language: 'sv-SE',
+      containerId:'dibs-complete-checkout',
+    };
+
+    useEffect(() => {
+      if(paymentIdFromURL) {
+        console.log(`Payment ID ${paymentIdFromURL} incoming in URL`);
+        // eslint-disable-next-line no-undef
+        const checkout = new Dibs.Checkout({...defaultCheckoutOptions, paymentId: paymentIdFromURL});
+  
+        checkout.on('pay-initialized', (response) => {
+          console.log('checkout on pay-initialized called');
+        });
+  
+        checkout.on('payment-completed', (response) => {
+          console.log('checkout on payment-completed called');
+          // Response: paymentId: string (GUID without dashes)
+          history.push('/thankyou', { paymentId: response.paymentId });
+        });
+      }
+    });
+    
 
     const createPayment = async () => {
         const res = await fetch('http://localhost:5000/api/checkout/getid', { method: 'POST'});
@@ -14,14 +39,12 @@ const Basket = () => {
         
         console.log('checkoutKey', process.env.REACT_APP_CHECKOUT_KEY);
         const checkoutOptions = {
-          checkoutKey: process.env.REACT_APP_CHECKOUT_KEY,
-          paymentId: paymentId,
-          language: 'sv-SE',
-          containerId:'dibs-complete-checkout',
+          ...defaultCheckoutOptions,
+          paymentId: paymentId,          
         };
     
         // eslint-disable-next-line no-undef
-        checkout = new Dibs.Checkout(checkoutOptions);
+        const checkout = new Dibs.Checkout(checkoutOptions);
     
         checkout.on('payment-completed', (response) => {
           console.log('checkout on payment-completed called');
