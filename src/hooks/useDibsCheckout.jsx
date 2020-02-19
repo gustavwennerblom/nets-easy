2133aec0bd289833f37e9faa8e125086d2d96c56
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2020-02-19 11:04:49 +0100

    Construct DIBS in a custom hook

---
+++ src/hooks/useDibsCheckout.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+export const useDibsCheckout = (paymentId, history) => {
+  const defaultCheckoutOptions = {
+    checkoutKey: process.env.REACT_APP_CHECKOUT_KEY,
+    language: 'sv-SE',
+    containerId:'dibs-complete-checkout',
+  };
+  
+  useEffect(() => {
+    if(paymentId) {
+    
+      // eslint-disable-next-line no-undef
+      const checkout = new Dibs.Checkout({...defaultCheckoutOptions, paymentId: paymentId});
+
+      // checkout.on('pay-initialized', (response) => {
+      //   console.log('checkout on pay-initialized called');
+      // });
+
+      checkout.on('payment-completed', (response) => {
+        console.log('checkout on payment-completed called');
+        // Response: paymentId: string (GUID without dashes)
+        history.push('/thankyou', { paymentId: response.paymentId });
+      });
+    }
+  });    
+}
--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { useDibsCheckout } from '../hooks/useDibsCheckout'
 
 const Basket = () => {
     const [paymentId, setPaymentId] = useState();
@@ -9,51 +10,28 @@ const Basket = () => {
     const searchParams = new URLSearchParams(history.location.search)
     const paymentIdFromURL = searchParams.get('paymentId')
 
-    const defaultCheckoutOptions = {
-      checkoutKey: process.env.REACT_APP_CHECKOUT_KEY,
-      language: 'sv-SE',
-      containerId:'dibs-complete-checkout',
-    };
-
     if(paymentIdFromURL && paymentIdFromURL !== paymentId) {
       console.log(`Payment ID ${paymentIdFromURL} incoming in URL`);
       setPaymentId(paymentIdFromURL)
     }
-    
-    // TODO: Could be own hook?
-    useEffect(() => {
-      if(paymentId) {
-      
-        // eslint-disable-next-line no-undef
-        const checkout = new Dibs.Checkout({...defaultCheckoutOptions, paymentId: paymentId});
-  
-        // checkout.on('pay-initialized', (response) => {
-        //   console.log('checkout on pay-initialized called');
-        // });
-  
-        checkout.on('payment-completed', (response) => {
-          console.log('checkout on payment-completed called');
-          // Response: paymentId: string (GUID without dashes)
-          history.push('/thankyou', { paymentId: response.paymentId });
-        });
-      }
-    });    
+
+    useDibsCheckout(paymentId, history);
 
     const createPayment = async () => {
-        const res = await fetch('http://localhost:5000/api/checkout/getid', { method: 'POST'});
-        const { paymentId: newPaymentId } = await res.json()
-        console.log('New payment with created with id ', newPaymentId);
-        setPaymentId(newPaymentId);        
+      const res = await fetch('http://localhost:5000/api/checkout/getid', { method: 'POST'});
+      const { paymentId: newPaymentId } = await res.json()
+      console.log('New payment with created with id ', newPaymentId);
+      setPaymentId(newPaymentId);        
     }
     
-      return (
-        <Container className="Basket">
-          <h1>DIBS Easy demo</h1>
-          <button id="CreatePayment" onClick={createPayment}>Pay for stuff</button>
-          {/* Add entry point for DIBS checkout */}
-          <div id="dibs-complete-checkout" />
-        </Container>
-      );
+    return (
+      <Container className="Basket">
+        <h1>DIBS Easy demo</h1>
+        <button id="CreatePayment" onClick={createPayment}>Pay for stuff</button>
+        {/* Add entry point for DIBS checkout */}
+        <div id="dibs-complete-checkout" />
+      </Container>
+    );
 };
 
 export default Basket;
